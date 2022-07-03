07bc81e5caf4d0aaa45d9ea24f67e611ec7e27f6
nakamuloud <25874783+nakamuloud@users.noreply.github.com>
2022-07-03 17:54:41 +0900

    chotfix

--- src/context.decorator.ts
+++ src/context.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const Context = createParamDecorator((ctx: ExecutionContext) => {
-  const handlerName = ctx.getHandler().name;
+  const handlerName = ctx.getClass().name;
   return handlerName;
 });
